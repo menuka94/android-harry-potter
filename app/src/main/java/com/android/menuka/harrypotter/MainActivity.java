cce2716f694df1b76dc434b798d9e64736519046
Menuka <menuka.14@cse.mrt.ac.lk>
2017-01-24 11:55:43 +0530

    Add Categories

--- app/src/main/java/com/android/menuka/harrypotter/MainActivity.java
+++ app/src/main/java/com/android/menuka/harrypotter/MainActivity.java
@@ -2,12 +2,39 @@ package com.android.menuka.harrypotter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> categories;
+    private ListView categoriesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        categoriesListView = (ListView)findViewById(R.id.categoriesListView);
+
+        categories = new ArrayList<>();
+
+        categories.add("Students");
+        categories.add("Professors");
+        categories.add("Houses");
+        categories.add("Magical Creatures");
+        categories.add("Subjects");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categories);
+        categoriesListView.setAdapter(arrayAdapter);
+
+        categoriesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("Tapped: " + categories.get(position));
+            }
+        });
     }
 }
